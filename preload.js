d8e4f232e6c9629574801944d3207d263d974c3a
uocraW <96836668+uocraW@users.noreply.github.com>
2026-04-22 19:21:38 +0800

    Updates

---
+++ preload.js
@@ -0,0 +1,43 @@
+/**
+ * Electron Preload Script
+ * Exposes safe IPC APIs to React renderer process
+ * Implements context isolation for security
+ */
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+/**
+ * Expose safe API object to React
+ * window.electronAPI will be available to renderer process
+ */
+contextBridge.exposeInMainWorld('electronAPI', {
+    /**
+     * Invoke IPC handler in main process
+     * @param {string} channel - Handler name
+     * @param {...args} args - Arguments to pass to handler
+     * @returns {Promise} Result from handler
+     */
+    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+
+    /**
+     * Listen for IPC events from main process
+     * @param {string} channel - Event name
+     * @param {function} callback - Handler function
+     * @returns {function} Function to remove listener
+     */
+    on: (channel, callback) => {
+        const listener = (event, ...args) => callback(...args);
+        ipcRenderer.on(channel, listener);
+        // Return function to remove listener
+        return () => ipcRenderer.removeListener(channel, listener);
+    },
+
+    /**
+     * Remove IPC event listener
+     * @param {string} channel - Event name
+     * @param {function} callback - Handler to remove
+     */
+    off: (channel, callback) => {
+        ipcRenderer.removeListener(channel, callback);
+    },
+});
